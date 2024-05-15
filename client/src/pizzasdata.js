6f7aa90b9b06c0c0c7673bc6eb3632c9040cc831
rajthanusan <rajthanusan08@gmail.com>
2024-05-15 21:54:03 +0530

    STATIC DATA added

---
+++ client/src/pizzasdata.js
@@ -0,0 +1,88 @@
+const pizzas = [
+  {
+    name: "PEPPER BARBECUE CHICKEN",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    description: "Pepper BarBecue Chicken I Cheese",
+  },
+  {
+    name: "MARGHERITA",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 150,
+        medium: 300,
+        large: 350,
+      },
+    ],
+    category: "veg",
+    image: "https://www.dominos.co.in/files/items/Margherit.jpg",
+    description: "Classic delight with 100% real mozzarella cheese",
+  },
+  {
+    name: "FARM HOUSE",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 250,
+        medium: 400,
+        large: 500,
+      },
+    ],
+    category: "veg",
+    image: "https://www.dominos.co.in/files/items/Farmhouse.jpg",
+    description:
+      "Delightful combination of onion, capsicum, tomato & grilled mushroom",
+  },
+  {
+    name: "PEPPY PANEER",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 220,
+        medium: 370,
+        large: 450,
+      },
+    ],
+    category: "veg",
+    image: "https://www.dominos.co.in/files/items/Peppy_Paneer.jpg",
+    description: "Chunky paneer with crisp capsicum and spicy red pepper",
+  },
+  {
+    name: "CHICKEN SAUSAGE",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 230,
+        medium: 380,
+        large: 460,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Chicken_Sausage.jpg",
+    description: "American classic! Spicy, herbed chicken sausage on pizza",
+  },
+  {
+    name: "VEGGIE PARADISE",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 240,
+        medium: 390,
+        large: 480,
+      },
+    ],
+    category: "veg",
+    image: "https://www.dominos.co.in/files/items/Veggie_Paradise.jpg",
+    description: "Goldern corn, black olives, capsicum, red paprika",
+  },
+];
+export default pizzas;
